8e53ca17046d73233ef4ac8536fa8ff95855a96c
Chris Fulstow <A134163@agl.com.au>
2016-03-01 16:40:00 +1100

    Add hero properties.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,9 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-component',
-    template: '<h1>Angularity Component</h1>'
+    template: '<h1>{{title}}</h1><h2>{{hero}} details!<h2>'
 })
 export class AppComponent {
-    
+    public title = 'Tour of Heros';
+    public hero = 'Windstorm';
 }
