211d60b0c8518d6cd1e505c1146b91111e839d2b
Chris Fulstow <A134163@agl.com.au>
2016-03-01 16:56:58 +1100

    Add hero input.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,25 @@
 import {Component} from 'angular2/core';
 
+interface Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-component',
-    template: '<h1>{{title}}</h1><h2>{{hero}} details!<h2>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>{{hero.name}}
+            <div><input value="{{hero.name}}" placeholder="name"></div>
+        </div>`
 })
 export class AppComponent {
     public title = 'Tour of Heros';
-    public hero = 'Windstorm';
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
 }
